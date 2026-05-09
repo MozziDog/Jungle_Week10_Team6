c6faba3efd4e8c6621952bd076debf958456a508
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-09 10:49:03 +0900

    Merged with main

--- CrashEngine/Source/Engine/Component/SkeletalMeshComponent.h
+++ CrashEngine/Source/Engine/Component/SkeletalMeshComponent.h
@@ -11,4 +11,20 @@ public:
 
 	FPrimitiveProxy* CreateSceneProxy() override;
     FSkeletalMeshBuffer* GetMeshBuffer() const override;
+    ~USkeletalMeshComponent() override = default;
+
+	void Serialize(FArchive& Ar) override;
+    void PostDuplicate() override;
+
+	void GetEditableProperties(TArray<FPropertyDescriptor>& OutProps) override;
+	void PostEditProperty(const char* PropertyName) override;
+
 };
+
+/*
+= scene proxy 생성
+= 에디터 property 노출
+= serialize/reload
+= tick 시 pose 갱신 호출
+= 나중에 animation instance 연결
+*/
